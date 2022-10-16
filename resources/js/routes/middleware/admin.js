670b10da9ee97abb4e9be26026ae7b28d12ab771
xatz <xatron3@gmail.com>
2022-10-17 00:20:29 +0200

    Fixed admin routes vue router

--- resources/js/routes/middleware/admin.js
+++ resources/js/routes/middleware/admin.js
@@ -1,9 +1,16 @@
 import store from "../../store/store";
 
 export default function auth(to, from, next) {
-  if (1 !== 1) {
-    next("/dashboard");
-  }
-
-  next();
+  store.dispatch("getUser").then(
+    (res) => {
+      if (res.is_admin === 1) {
+        next();
+      } else {
+        next("/dashboard");
+      }
+    },
+    (err) => {
+      next("/dashboard");
+    }
+  );
 }
--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,4 +1,6 @@
 import { createStore } from "vuex";
+import { getUser } from "../services/auth";
+import { router } from "../routes/index";
 
 // Create a new store instance.
 const store = createStore({
@@ -22,6 +24,29 @@ const store = createStore({
     },
     toggleNavigation(state) {
       state.settings.showNavigation = !state.settings.showNavigation;
+    },
+  },
+  actions: {
+    getUser(context, data) {
+      return new Promise(async (resolve, reject) => {
+        if (localStorage.getItem("access_token")) {
+          if (context.state.user === null) {
+            let res = await getUser();
+
+            if (res !== undefined) {
+              context.commit("setUser", res.data);
+              resolve(res.data);
+            } else {
+              localStorage.removeItem("access_token");
+              reject("No user found");
+            }
+          } else {
+            resolve(context.state.user);
+          }
+        } else {
+          reject("No access token found");
+        }
+      });
     },
   },
 });
