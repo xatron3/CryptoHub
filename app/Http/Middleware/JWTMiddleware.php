dec010d2e125b2d508e6c2a4594c87579b2e149e
xatz <xatron3@gmail.com>
2022-11-03 21:23:24 +0100

    Heroku test

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -3,9 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Exception;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JWTMiddleware
@@ -26,6 +27,7 @@ class JWTMiddleware
         return response()->json(['message' => 'Token is Invalid']);
       } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
         $newToken = JWTAuth::parseToken()->refresh();
+        Log::error($newToken);
         return response()->json(['message' => 'Token Expired', 'token' => $newToken], 401);
       } else {
         return response()->json(['message' => 'Authorization Token not found']);
