348dcb8e867d0447fb2a19ad85bbba49d48ecab8
xatz <xatron3@gmail.com>
2022-12-30 05:13:58 +0100

    Assets table fix

--- resources/js/pages/Public/Home/table/assets.vue
+++ resources/js/pages/Public/Home/table/assets.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="w-full overflow-x-auto">
+    {{ this.$store.getters["assets/pageInfo"].page }}/{{
+      this.$store.getters["assets/pageInfo"].totalPages
+    }}
+
     <table class="w-full shadow-md rounded-lg overflow-hidden">
       <thead>
         <tr
@@ -7,9 +11,11 @@
         >
           <th class="p-2 md:p-4">Name</th>
           <th class="p-2 md:p-4">Price</th>
-          <th class="p-2 md:p-4" @click="sortOnMarketcap">Marketcap</th>
+          <th class="p-2 md:p-4 cursor-pointer" @click="sortOnMarketcap">
+            Marketcap
+          </th>
           <th
-            class="p-2 md:p-4 hidden md:table-cell"
+            class="p-2 md:p-4 hidden md:table-cell cursor-pointer"
             @click="sortOnPercentageChange"
           >
             24H Change
@@ -19,7 +25,7 @@
 
       <tbody>
         <tr
-          v-for="(asset, index) in this.assets"
+          v-for="(asset, index) in this.$store.getters['assets/allFiltered']"
           :key="asset.name"
           :class="{
             'dark:bg-gray-750 bg-gray-300': index % 2 === 0,
@@ -64,7 +70,7 @@
           <td
             class="text-xs md:text-base px-1 py-1 md:px-4 md:py-3 font-semibold text-black dark:text-white"
           >
-            {{ marketCap(asset) }}
+            ${{ marketCap(asset) }}
           </td>
 
           <!-- Price Change -->
@@ -80,22 +86,28 @@
         </tr>
       </tbody>
     </table>
+
+    <div class="mt-3 mx-auto rounded-md overflow-hidden flex justify-center">
+      <div
+        v-for="index in this.$store.getters['assets/pageInfo'].totalPages"
+        v-bind:key="index"
+        class="px-2 py-1 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-900 transition-all"
+        :class="{
+          'bg-gray-200 dark:bg-gray-600':
+            this.$store.getters['assets/pageInfo'].page === index,
+          'bg-gray-100 dark:bg-gray-700':
+            this.$store.getters['assets/pageInfo'].page !== index,
+        }"
+        v-on:click="setPage(index)"
+      >
+        <span>{{ index }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["data"],
-  data() {
-    return {
-      sort: "",
-      assets: {},
-    };
-  },
-  computed: {},
-  mounted() {
-    this.assets = this.$store.getters["assets/marketcap"];
-  },
   methods: {
     marketCap(asset) {
       let market_cap;
@@ -111,18 +123,18 @@ export default {
     price(asset) {
       return nums.formatPrice(asset.current_price);
     },
+    setPage(page) {
+      this.$store.commit("assets/setPage", page);
+    },
     sortOnPercentageChange() {
-      if (this.sort === "gainers") {
-        this.sort = "loosers";
-        this.assets = this.$store.getters["assets/loosers"];
+      if (this.$store.getters["assets/currentSorting"] === "gainers") {
+        this.$store.commit("assets/setSorting", "loosers");
       } else {
-        this.assets = this.$store.getters["assets/gainers"];
-        this.sort = "gainers";
+        this.$store.commit("assets/setSorting", "gainers");
       }
     },
     sortOnMarketcap() {
-      this.sort = "marketcap";
-      this.assets = this.$store.getters["assets/marketcap"];
+      this.$store.commit("assets/setSorting", "marketcap");
     },
   },
 };
