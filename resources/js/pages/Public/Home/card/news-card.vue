d531dff0c51e9513a0c686b3b0b64b3b09238542
xatz <xatron3@gmail.com>
2023-01-04 02:30:21 +0100

    Home page featured posts

--- resources/js/pages/Public/Home/card/news-card.vue
+++ resources/js/pages/Public/Home/card/news-card.vue
@@ -2,26 +2,43 @@
   <div
     class="w-full md:max-w-sm rounded overflow-hidden shadow-lg bg-gray-600 flex flex-col"
   >
-    <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">{{ firstPost.title }}</div>
-      <div class="text-gray-100 text-base">
-        {{ firstPost.content }}
+    <div
+      v-for="(post, index) in featuredPosts"
+      v-bind:key="post.id"
+      :class="{ hidden: index !== currentIndex }"
+    >
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ post.title }}</div>
+        <div class="text-gray-100 text-base">
+          {{ post.content }}
+        </div>
+        <a :href="post.slug" class="mt-1 text-xs">Read More</a>
       </div>
-      <a :href="firstPost.slug" class="mt-1 text-xs">Read More</a>
+      <div class="px-6 pb-2 mt-auto">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#FTX</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#crypto</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#fruad</span
+        >
+      </div>
     </div>
-    <div class="px-6 pb-2 mt-auto">
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#FTX</span
-      >
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#crypto</span
-      >
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#fruad</span
-      >
+
+    <div class="flex justify-center space-x-1">
+      <div v-for="index in featuredPosts.length" v-bind:key="index">
+        <div
+          @click="currentIndex = index - 1"
+          :class="{ 'text-green-500': currentIndex === index - 1 }"
+        >
+          *
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,9 +46,14 @@
 <script>
 export default {
   computed: {
-    firstPost() {
-      return this.$store.getters["posts/get"](3);
+    featuredPosts() {
+      return this.$store.getters["posts/all"].slice(0, 4);
     },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
   },
 };
 </script>
